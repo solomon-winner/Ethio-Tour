bf604022ed11f3c9bf2d342c9fe33e1ee8bce152
solomon-winner <solyalew3@gmail.com>
2024-10-05 12:40:18 +0300

    ...big cards json file...

--- src/components/bigCards/Big_cards.jsx
+++ src/components/bigCards/Big_cards.jsx
@@ -3,7 +3,11 @@ import "./Big_Cards.css";
 const Big_Cards = () => {
 return (
     <div className="big_cards">
-
+        <img src="" alt="simien mountain image" />
+        <div className="card_desc">
+            <p className="card_title">Title</p>
+            <p className="card_text">Description</p>
+        </div>
     </div>
 );
 }
