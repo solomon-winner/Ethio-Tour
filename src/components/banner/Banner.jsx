5e8bc29c22e6d9ae27da157ee32e2d887a0e19d1
solomon-winner <solyalew3@gmail.com>
2024-10-05 11:07:15 +0300

    navigation bar

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,5 +1,13 @@
 const Banner = () => {
-
+    return (
+        <div className="banner">
+            <div className="banner__content">
+                <h1>Explore Ethiopia</h1>
+                <p>Discover The Land of Wonders!</p>
+                <button>Learn More</button>
+                </div>
+        </div>
+    );
 }
 
 export default Banner;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import "./Navbar.css";
+import Logo from "../../assets/logo.png";
+
 const Navbar = () => {
     return(
         <div className="navbar">
             <div className="navbar__left">
-                <img src="" alt="" />
+                <img src={Logo} alt="Ethio Tour logo " />
             </div>
             <div className="navbar__right">
                 <ul>
