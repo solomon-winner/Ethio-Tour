23fe994f536f560b39adc47c5f0ade0618cfd658
solomon-winner <solyalew3@gmail.com>
2024-10-05 14:33:32 +0300

    ...

--- src/components/bigCards/BigCards.jsx
+++ src/components/bigCards/BigCards.jsx
@@ -1,7 +1,26 @@
 import "./BigCards.css";
-import bigCardsData from "../../utils/big_cards.json";
+import Semien from "../../assets/images/semen.PNG";
+import Fassil from "../../assets/images/fassil.PNG";
+import Irrecha from "../../assets/images/Irrecha.PNG";
 
 const Big_Cards = () => {
+    const bigCardsData = [
+        {
+        "image": Semien,
+        "title": "Breathtaking Landscapes",
+        "description": "Explore the diverse landscapes of Ethiopia, from the Simien Mountains to the Danakil Depression."
+        },
+        {
+        "image": Fassil,
+        "title": "Historical Sites",
+        "description": "Delve into the ancient history of Ethiopia by visiting iconic sites such as Lalibela and Gondar."
+        },
+        {
+        "image": Irrecha,
+        "title": "Cultural Festivals",
+        "description": "Immerse yourself in the colorful celebrations and traditions of Ethiopia, including the Timkat Festival, Meskel and Irrecha."
+        }
+    ]
 return (
  <div className="cards-wrapper">
     { bigCardsData.map((card, index) => (
