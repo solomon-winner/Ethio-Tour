c6ca5d8c2584672d15338c8c5e29bae4988c1b52
solomon-winner <solyalew3@gmail.com>
2024-10-05 09:07:35 +0300

    ...setup the project...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import { Routes, Route} from 'react-router-dom';
+import Home from './pages/home/Home.jsx';
+import About from './pages/about/About.jsx';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path = '/' element={<Home />} />
+        <Route path = '/about' element={<About />} />
+      </Routes>
     </div>
   );
 }
