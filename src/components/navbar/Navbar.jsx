d04662d031032c8017790a2d83464962995f830c
solomon-winner <solyalew3@gmail.com>
2024-10-05 10:27:50 +0300

    ...images...

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,18 @@
+import "./Navbar.css";
 const Navbar = () => {
-
+    return(
+        <div className="navbar">
+            <div className="navbar__left">
+                <img src="" alt="" />
+            </div>
+            <div className="navbar__right">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
